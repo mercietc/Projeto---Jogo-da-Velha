3b1fc7375ec779eb98cd48eb42a19d4e14c8db73
SariaBarros <sandy.vitoriabarros@gmail.com>
2022-09-30 18:36:30 -0400

    codigo modularizado

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,52 +1,36 @@
-package principal;
+package tictactoe;
 import java.util.Scanner;
 
 public class Main {
-
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         String jogador1;
         String jogador2;
         int opcaoEscolhida;
 
         //Iniciando jogo e exibindo menu
-        System.out.println("\nIniciando o Jogo da Velha...\n");
-        System.out.println("----- Boas vindas ao Jogo da Velha! -----\n");
-        System.out.println(" _______________________________________");
-        System.out.println("|__________________Menu_________________|");
-        System.out.println("|                                       |");
-        System.out.println("|  Jogador 1: X                         |");
-        System.out.println("|  Jogador 2: O                         |");
-        System.out.println("|____________________*__________________|");
+        menu();
 
         //Entrada dos nomes dos jogadores
-        System.out.println("-----------------------------------------");
-        sc.nextLine();
-        System.out.println("  Digite o nome do Jogador 1:            ");
-        jogador1 = sc.next();
-        jogador1 = jogador1.toUpperCase();
-        System.out.println("-----------------------------------------");
-        sc.nextLine();
-        System.out.println("  Digite o nome do Jogador 2:            ");
-        jogador2 = sc.next();
-        jogador2 = jogador2.toUpperCase();
+        jogador1 = getNome("Jogador 1");
+        jogador2 = getNome("Jogador 2");
+
         System.out.println("-----------------------------------------");
 
-        //Iniciando partida e escolha das jogadas
-        while (true) {
-            System.out.println(" ---* Escolha uma posição no tabuleiro para jogar *---");
-
-            //Criando e exibindo o tabuleiro
-            int contador = 1;
-            int[][] tabuleiro = new int[3][3];
-            for (int i = 0; i < tabuleiro.length; i++) {
-                for (int j = 0; j < tabuleiro[i].length; j++) {
-                    tabuleiro[i][j] = contador;
-                    System.out.print(tabuleiro[i][j] + " ");
-                    contador += 1;
-                }
-                System.out.println();
+        //Iniciando a matriz
+        int[][] tabuleiro = new int[3][3];
+        int contador = 1;
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                tabuleiro[i][j] = contador;
+                contador += 1;
             }
+        }
+        //Chamar o tabuleiro
+        mostrarTabuleiro(tabuleiro);
+        //Iniciando partida e escolha das jogadas
+
 
             //Escolha da posicao
             System.out.println("Digite o número correspondente à posição que deseja jogar: ");
@@ -60,6 +44,31 @@ public class Main {
                     }
                 }
             }
+        }
+
+    static void menu(){
+        System.out.println("\nIniciando o Jogo da Velha...\n");
+        System.out.println("----- Boas vindas ao Jogo da Velha! -----\n");
+        System.out.println(" _______________________________________");
+        System.out.println("|__________________Menu_________________|");
+        System.out.println("|                                       |");
+        System.out.println("|  Jogador 1: X                         |");
+        System.out.println("|  Jogador 2: O                         |");
+        System.out.println("|____________________*__________________|");
+    }
+    static String getNome(String io){
+        //TODO: tratar excessão.
+        System.out.println("-----------------------------------------");
+        System.out.printf("  Digite o nome do %s:            \n", io);
+        return sc.next();
+
+    }
+    static void mostrarTabuleiro(int[][] tabuleiro){
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print(tabuleiro[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
